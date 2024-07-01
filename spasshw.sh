932f1f3790cfe2efa490cff2165c31bb828e356a
Vellisca <146184942+Vellisca@users.noreply.github.com>
2024-07-01 12:32:59 +0200

    Update spasshw.sh

--- spasshw.sh
+++ spasshw.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Pedir el nombre de usuario
 read -p "Introduce tu nombre de usuario: " USER
-
-# Pedir la contraseña (la entrada no será visible)
 read -s -p "Introduce tu contraseña: " PASSWORD
 echo
 
-# Configurar el dominio y el servidor
 DOMAIN="durum.es"
 SERVER="lt$USER"
 
