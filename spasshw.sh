67d396d820715c190c8cffbdff46d2e7aa712fb8
Vellisca <146184942+Vellisca@users.noreply.github.com>
2024-07-01 12:29:55 +0200

    Update spasshw.sh

--- spasshw.sh
+++ spasshw.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-read -p "Ingrese su nombre de usuario: " username
-read -s -p "Ingrese su contraseña: " password
-remmina_file=$(mktemp /tmp/remmina_XXXXXX.remmina)
+# Pedir el nombre de usuario
+read -p "Introduce tu nombre de usuario: " USER
 
-server="lt+$username"
-domain="tu_dominio_fijo"
-remmina_file=$(mktemp /tmp/remmina_XXXXXX.remmina)
+# Pedir la contraseña (la entrada no será visible)
+read -s -p "Introduce tu contraseña: " PASSWORD
+echo
 
-cat << EOF > "$remmina_file"
+# Configurar el dominio y el servidor
+DOMAIN="durum.es"
+SERVER="lt$USER"
+
+# Crear un archivo de configuración temporario para remmina
+CONFIG_FILE=$(mktemp /tmp/remmina_XXXXX.remmina)
+
+cat <<EOF > $CONFIG_FILE
 [remmina]
-username=$username
-password=$password
-domain=$domain
-server=$server
+username=$DOMAIN\\$USER
+password=$PASSWORD
+server=$SERVER
 protocol=RDP
 EOF
 
-# Ejecuta remmina con el archivo de configuración
-remmina -c "$remmina_file"
-rm "$remmina_file"
+# Ejecutar remmina con el archivo de configuración
+remmina -c $CONFIG_FILE
+
+# Eliminar el archivo de configuración temporario después de la conexión
+rm $CONFIG_FILE
