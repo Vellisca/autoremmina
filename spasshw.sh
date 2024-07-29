2247052f6d92b139fee151496d68a1eeb708494e
Vellisca <146184942+Vellisca@users.noreply.github.com>
2024-07-29 14:34:56 +0200

    spasshw.sh with no vuln

--- spasshw.sh
+++ spasshw.sh
@@ -6,7 +6,7 @@ read -s -p "Introduce tu contraseña: " PASSWORD
 echo
 
 DOMAIN="gmv.es"
-SERVER="lt$USER"
+SERVER="lt${USER}.gmv.es"
 
 # Ejecutar remmina con los parámetros de conexión
-remmina -c "rdp://$DOMAIN\\$USER:$PASSWORD@$SERVER"
+remmina -c "rdp://$DOMAIN\\$USER:$'PASSWORD'@$SERVER"
