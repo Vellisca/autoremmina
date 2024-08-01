7137ef9f48b70bda1eb9935b56f69652513cf92e
Vellisca <146184942+Vellisca@users.noreply.github.com>
2024-08-01 14:47:57 +0200

    Commit final :)

--- spasshw.sh
+++ spasshw.sh
@@ -2,6 +2,7 @@
 
 echo "RECUERDA HABILITAR RDP EN EL EQUIPO CORPORATIVO"
 read -p "Introduce tu nombre de usuario: " USER
+#PASS=$(printf '%s' "$PASSWORD" | od -An -tx1 | tr -d ' \n' | sed 's/\(..\)/%1/g')
 PASSWORD= "0"
 echo
 DOMAIN="gmv.es"
